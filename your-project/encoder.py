30a4aee437c915eed95bbccf15a9ec0e1692e9ad
Oriol Cunill <cunillati@gmail.com>
2020-01-16 12:23:53 +0100

    NOT A VALID PATH saving this for a future when transforming a str into an int passing through bytes, system loses information

--- your-project/encoder.py
+++ your-project/encoder.py
@@ -4,22 +4,54 @@ from datetime import datetime
 
 class Encoder:
 
+    def split_string_in_parts(self, msg):
+        spl = []
+        for i in range(0, len(msg), 2):
+            splitted_part = msg[i:i + 2]
+            print('splitted part', splitted_part)
+            splitted_bytes = bytes(splitted_part, 'utf-8')
+            print('splitted bytes', splitted_bytes)
+            int_val = int.from_bytes(splitted_bytes, byteorder = 'big')
+            spl.append(int_val)
+        print('splitted -->',spl)
+        return spl
+
     def encode_str(self, msg, encriptor):
-        encoded_bytes = base64.b64encode(msg.encode("utf-8"))
-        encoded_str = str(int.from_bytes(encoded_bytes, byteorder = 'big'), "utf-8")
-        separator = len(encoded_str) / 2
-        encoded_parts = [encoded_str[:separator], encoded_str[separator:]]
+        encoded_bytes = base64.b64encode(msg.encode('utf-8'))
+        print('encoded bytes', encoded_bytes)
+        encoded_str = str(encoded_bytes, 'utf-8')
+        print('encoded str', encoded_str)
+        encoded_parts = self.split_string_in_parts(encoded_str)
         return [int(i) * encriptor for i in encoded_parts]
     
     def decode_str(self, msg, encriptor):
-        # TODO: ERROR --> split msg string in 2 instead of casting to int
-        """msg_bytes = bytes(int(msg / encriptor))
-        encodedStr = str(msg_bytes, "utf-8")
-        decodedBytes = base64.b64decode(encodedStr)
-        return str(decodedBytes, "utf-8")
+        encoded_list = []
+        for i in msg:
+            #print('value i: ', i, ' value encriptor', encriptor, ' result', (i/encriptor))
+            base_int = int(i / encriptor)
+            #print('base_int',base_int)
+            base_bytes = bytes(base_int)
+            #print('base_bytes', base_bytes)
+            base_str = str(base_bytes,'utf-8')
+            encoded_list.append(base_str)
+
+        print(encoded_list)
+        encoded_str = ''.join(encoded_list)
+        print('encoded_str', encoded_str)
+        encoded_bytes = bytes(encoded_str, 'utf-8')
+        decoded_bytes = base64.b64decode(encoded_bytes)
+        return str(decoded_bytes, 'utf-8')
+
+
+
         """
-        encoded_int = sum([i / 2 for i in msg])
-        encoded_bytes = bytes(encoded_int)
+        encoded_list = [str(int(i / encriptor)) for i in msg]
+        encoded_bytes = bytes(int(''.join(encoded_list)))
+        print('decoding bytes', encoded_bytes)
+        encoded_str = str(encoded_bytes, 'utf-8')
+        print('decoding string', encoded_str)
+        decoded_bytes = base64.b64decode(encoded_bytes)
+        return str(decoded_bytes, 'utf-8')"""
 
 class MessageManager:
     messages = []
@@ -38,8 +70,8 @@ class MessageManager:
 
     def add_message(self, msg):
         print('adding message: ', msg)
-        print('encriptor: ', self.encriptors[-1])
-        self.messages.append(self.encoder.encode_str(msg, self.encriptors[-1]))
+        print('encriptor: ', self.encriptors[0])
+        self.messages.append(self.encoder.encode_str(msg, self.encriptors[0]))
         self.add_new_encriptor()
 
     def read_message(self, pos):
